eb395b11e7af7c6de660524ba44e257ac5f0191a
VincentPinet <vincent.pinet@gmail.com>
2019-12-24 18:44:22 +0100

    Major code restructuring

--- 421.cpp
+++ 421.cpp
@@ -1,47 +1,34 @@
-#include <iostream>
-#include <cstdio>
-#include <cmath>
+#include <algorithm>
 #include <functional>
-#include <vector>
+#include <iostream>
 #include <list>
-#include <unordered_map>
 #include <random>
+#include <unordered_map>
 
 #define N_ROLL 3
 #define N_DICE 3
-#define DICE distribution(engine)
 
 using namespace std;
 
 typedef struct {
-	float ev;
+	float val;
 	int action = 0;
 } entry;
 
-unordered_map<int, unordered_map<int, unordered_map<int, entry>>> d;
-unordered_map<int, unordered_map<int, entry>> z;
+unordered_map<int, unordered_map<int, entry>> f;
+unordered_map<int, unordered_map<int, unordered_map<int, entry>>> g;
 
-default_random_engine engine;
-uniform_int_distribution<int> distribution(1,6);
+vector<int> ranking = {
+	421,111,611,666,511,555,411,444,311,333,211,222,654,543,
+	432,321,665,664,663,662,661,655,653,652,651,644,643,642,
+	641,633,632,631,622,621,554,553,552,551,544,542,541,533,
+	532,531,522,521,443,442,441,433,431,422,332,331,322,221
+};
 
-vector<int> ranking = {421,111,611,666,511,555,411,444,311,333,211,222,654,543,432,321,
-					   665,664,663,662,661,655,653,652,651,644,643,642,641,633,632,631,
-					   622,621,554,553,552,551,544,542,541,533,532,531,522,521,443,442,
-					   441,433,431,422,332,331,322,221};
-
-
-float eval(int f, int s) {
-	if (f == s) return 0.5;
-	for (int r : ranking)
-		if (f == r) return 1;
-		else if (s == r) return 0;
-	return -1;
-}
-
-int canonicalize(int n) {
+int canonicalize(int r) {
 	int res = 0;
 	for (int k = 6; k >= 1; k--) {
-		for (int tmp = n; tmp > 0; tmp/=10) {
+		for (int tmp = r; tmp > 0; tmp/=10) {
 			if (tmp % 10 == k) {
 				res *= 10;
 				res += tmp % 10;
@@ -56,80 +43,76 @@ int replace(int r, int i, int d) {
 	return r - (r / tmp % 10) * tmp + d * tmp;
 }
 
-list<int> outcomes(int r, int a) {
-	if (r == 221) return {};
+void update(entry &min, entry o, function<bool(float,float)> cmp) {
+	if (cmp(o.val, min.val)) {
+		min.val = o.val;
+		min.action = o.action;
+	}
+}
+
+list<int> outcomes(int r = 0, int a = 0b111) {
+	// if (r == 221) return {}; // rule variant
 	list<int> res = {r};
-	for (int i  = 0; i < N_DICE; i++) {
+	for (int i = 0; i < N_DICE; i++) {
 		if ((a >> i) & 1) {
 			int end = res.size();
 			for (int s = 0; s < end; s++) {
 				int curr = res.front();
 				res.pop_front();
-				for (int d = 1; d <= 6; d++) {
-					res.push_back(canonicalize(replace(curr, i, d)));
-				}
+				for (int d = 1; d <= 6; d++)
+					res.push_back(replace(curr, i, d));
 			}
 		}
 	}
+	transform(res.begin(), res.end(), res.begin(), &canonicalize);
 	return res;
 }
 
-float gensum(int m, int o, int t, int a, function< float(int, int, int)> f) {
-	float sum = 0;
-	int total = 0;
-	for (int r : outcomes(m, a)) {
-		sum += f(r, o, t);
-		total++;
-	}
-	return sum / total;
+float ev(list<int> X, function< float(float, int)> f) {
+	return accumulate(X.begin(), X.end(), 0.0, f) / X.size();
 }
 
-void compute_a() {
-	for (int f : ranking)
-		z[f][2].ev = gensum(0, f, 2, 7, [] (int m, int o, int t) { return 1 - d[o][m][t].ev; });
+float eval(int a, int b) {
+	if (a == 221) return 0;
+	if (a == b) return 0.5;
+	for (int r : ranking)
+		if (a == r) return 1;
+		else if (b == r) return 0;
+	return -1;
+}
 
-	for (int f : ranking) {
-		for (int t = 1; t >= 0; t--) {
-			float curr = gensum(0, f, t, 7, [] (int m, int o, int t) { return 1 - d[o][m][t].ev; });;
-			int curr_a = 0;
-			for (int a = 1; a < 8; a++) {
-				float sum = gensum(f, 0, t, a, [] (int m, int o, int t) { return z[m][t+1].ev; });
-				if (sum > curr) {
-					curr = sum;
-					curr_a = a;
-				}
-			}
-			z[f][t].ev = curr;
-			z[f][t].action = curr_a;
+void compute_f() {
+	for (int r : ranking)
+		f[r][N_ROLL - 1].val = ev(outcomes(), [r] (float sum, float xi) { return sum + g[r][xi][N_ROLL - 1].val; });
+
+	for (int t = 1; t >= 0; t--) {
+		for (int r : ranking) {
+			entry max = { ev(outcomes(), [r, t] (float sum, float xi) { return sum + g[r][xi][t].val; }) };
+			for (int a = 0b001; a <= 0b111; a++)
+				update(max, {ev(outcomes(r, a), [t] (float sum, float xi) { return sum + f[xi][t+1].val; }), a}, greater<float>());
+			f[r][t] = max;
 		}
 	}
 }
 
-void compute_d() {
-	for (int f : ranking)
-		for (int s : ranking)
-			d[f][s][0].ev = eval(s, f);
+void compute_g() {
+	for (int r1 : ranking)
+		for (int r2 : ranking)
+			g[r1][r2][0].val = eval(r1, r2);
 
-	for (int f : ranking) {
-		for (int s : ranking) {
-			for (int t = 1; t < N_ROLL; t++) {
-				float curr = eval(s, f);
-				int curr_a = 0;
-				for (int a = 1; a < 8; a++) {
-					float sum = gensum(s, f, t, a, [] (int m, int o, int t) { return d[o][m][t-1].ev; });
-					if (sum > curr) {
-						curr = sum;
-						curr_a = a;
-					}
-				}
-				d[f][s][t].ev = curr;
-				d[f][s][t].action = curr_a;
+	for (int t = 1; t < N_ROLL; t++) {
+		for (int r1 : ranking) {
+			for (int r2 : ranking) {
+				entry min = {eval(r1, r2)};
+				for (int a = 0b001; a <= 0b111; a++)
+					update(min, {ev(outcomes(r2, a), [r1, t] (float sum, float xi) { return sum + g[r1][xi][t-1].val; }), a}, less<float>());
+				g[r1][r2][t] = min;
 			}
 		}
 	}
 }
 
-string action_to_string(int a, int r) {
+string action_to_string(int r, int a) {
 	string res = "";
 	for (int i = N_DICE - 1; i >= 0; i--)
 		if (!((a >> i) & 1))
@@ -137,56 +120,49 @@ string action_to_string(int a, int r) {
 	return res;
 }
 
+uniform_int_distribution<int> die(1,6);
+default_random_engine roll;
+
 int apply_action(int r, int a) {
 	int res;
 	for (int i  = 0; i < N_DICE; i++)
 		if ((a >> i) & 1)
-			res = replace(r, i, DICE);
+			res = replace(r, i, die(roll));
 	return canonicalize(res);
 }
 
 float sim() {
 	int t = 0;
-	int f = canonicalize(DICE * 100 + DICE * 10 + DICE);
 
-	// cout << "P1: " << f << " (" << t << ")" << endl;
-	for (;z[f][t].action; t++)
-		f = apply_action(f, z[f][t].action);
+	int r = canonicalize(die(roll) * 100 + die(roll) * 10 + die(roll));
+	for ( ;f[r][t].action; t++)
+		r = apply_action(r, f[r][t].action);
 
-	int s = canonicalize(DICE * 100 + DICE * 10 + DICE);
-	// cout << "P2: " << s << " (" << t << ")" << endl;
-	for (;d[f][s][t].action; t--) {
-		s = apply_action(s, d[f][s][t].action);
-	}
+	int s = canonicalize(die(roll) * 100 + die(roll) * 10 + die(roll));
+	for ( ;g[r][s][t].action; t--)
+		s = apply_action(s, g[r][s][t].action);
 
-	return eval(f, s);
+	return eval(r, s);
 }
+
+void print_f() {
+	cout << "    |  0           |  1           |  2           |" << endl;
+	cout << "    | proba |action| proba |action| proba |action|" << endl;
+	for (int r : ranking) {
+		cout << r << " | ";
+		for (int t = 0; t < N_ROLL; t++)
+			printf("%5.2f%% (%3s) | ", f[r][t].val*100, action_to_string(r, f[r][t].action).c_str());
+		cout << endl;
+	}
+}
+
 
 int main(int argc, char const * argv[]) {
 
-	compute_d();
-	compute_a();
-
-	cout << "    |  0           |  1           |  2           |\n";
-	for (int f : ranking) {
-		cout << f << " | ";
-		for (int t = 0; t < N_ROLL; t++)
-			printf("%.4f (%3s) | ", z[f][t].ev, action_to_string(z[f][t].action, f).c_str());
-		cout << endl;
-	}
-
-	float ev = 0;
-	int total = 0;
-	for (int f : outcomes(0, 7)) {
-		ev += z[f][0].ev;
-		total++;
-	}
-	cout << "TH  : " << ev / total << endl;
-
-	float res = 0;
-	for (int i = 0; i <10000000; i++)
-		res += sim();
-	cout << "SIM : " << res/10000000 << endl;
+	compute_g();
+	compute_f();
+	print_f();
+	cout << ev(outcomes(), [] (float sum, int xi) { return sum + f[xi][0].val; }) << endl;
 
 	return 0;
 }
